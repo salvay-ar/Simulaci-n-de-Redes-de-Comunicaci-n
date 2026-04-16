8d1440dc1d3a0b2a06bf74bdfcbaf0344881dd79
salvay-ar <arianasalvay@gmail.com>
2026-04-16 11:04:03 -0300

    primer commit

---
+++ simulacion_de_redes.py
@@ -0,0 +1,39 @@
+class Nodo:
+
+    def __init__(self, tipo):
+        self.tipo = tipo
+        self.conexiones = [] 
+
+    def agregar_conexion(self, nodo):
+        self.conexiones.append(nodo) # Se guarda en el servidor una lista con los clientes
+
+    def enviar_mensaje(self, mensaje):
+        print(f"{self.tipo} está enviando el mensaje: '{mensaje}'")  # Recibe el tipo y el mensaje
+
+        for conexion in self.conexiones:  # Busca cada conexion guardada en las conexiones de la clase que envio el mensaje
+             conexion.recibir_mensaje(mensaje, self.tipo)  # Cada una de las clases conectadas llama al otro método 
+                                                           #(recibir_mensaje), envía el mensaje y quién lo envió
+    
+    def recibir_mensaje(self, mensaje, remitente): 
+        print(f"{self.tipo} recibió el mensaje de {remitente}: '{mensaje}'") 
+
+
+#Se crea cada clase
+servidor = Nodo("Servidor")
+cliente1 = Nodo("Cliente1")
+cliente2 = Nodo("Cliente2")
+cliente3 = Nodo("Cliente3")
+
+#Conecta el servidor a todos los clientes
+servidor.agregar_conexion(cliente1)
+servidor.agregar_conexion(cliente2)
+servidor.agregar_conexion(cliente3)
+
+#Conectar los clientes al servidor (Para que también puedan enviar mensajes)
+cliente1.agregar_conexion(servidor)
+cliente2.agregar_conexion(servidor)
+cliente3.agregar_conexion(servidor)
+
+servidor.enviar_mensaje("Hola clientes")
+        
+                    
